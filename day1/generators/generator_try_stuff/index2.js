bdf5042018ea6c2efb376a09e7f31855c0aceae3
tamartwe <tamar.twena@gmail.com>
2018-04-21 02:07:24 +0300

    more changes

--- day1/generators/generator_try_stuff/index2.js
+++ day1/generators/generator_try_stuff/index2.js
@@ -30,3 +30,32 @@ var getUsersAndCategories = (cb) => {
     })
 }
 
+
+
+var b = {
+    p: 2,
+    q: "something else",
+    r: true
+}
+ 
+var c = {
+    p: 10,
+    q: "other things",
+    r: false
+}
+
+
+
+function Some(p, q, r){
+    this.p = p;
+    this.q = q;
+    this.r = r;
+}
+ 
+var a = new Some(1, "something", false);
+var b = new Some(2, "something else", true);
+var c = new Some(10, "other things", false);
+
+
+
+
